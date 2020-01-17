a5ec274fb828aa5685ca5df65e207f5861add5e9
ccsosa24 <chrstina.sosa024@gmail.com>
2020-01-16 21:13:54 -0700

    updateservieworker

--- friends2-clicky-game/src/serviceWorker.js
+++ friends2-clicky-game/src/serviceWorker.js
@@ -153,29 +153,29 @@ export function unregister() {
 //  * See https://goo.gl/2aRDsh
 //  */
 
-importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
+// importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
-importScripts(
-  "/Friends2-Clicky-Game/precache-manifest.0a5fe5bcacf5ddcc867e83ae90290669.js"
-);
+// importScripts(
+//   "/Friends2-Clicky-Game/precache-manifest.0a5fe5bcacf5ddcc867e83ae90290669.js"
+// );
 
-self.addEventListener('message', (event) => {
-  if (event.data && event.data.type === 'SKIP_WAITING') {
-    self.skipWaiting();
-  }
-});
+// self.addEventListener('message', (event) => {
+//   if (event.data && event.data.type === 'SKIP_WAITING') {
+//     self.skipWaiting();
+//   }
+// });
 
-workbox.core.clientsClaim();
+// workbox.core.clientsClaim();
 
-/**
- * The workboxSW.precacheAndRoute() method efficiently caches and responds to
- * requests for URLs in the manifest.
- * See https://goo.gl/S9QRab
- */
-self.__precacheManifest = [].concat(self.__precacheManifest || []);
-workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
+// /**
+//  * The workboxSW.precacheAndRoute() method efficiently caches and responds to
+//  * requests for URLs in the manifest.
+//  * See https://goo.gl/S9QRab
+//  */
+// self.__precacheManifest = [].concat(self.__precacheManifest || []);
+// workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/Friends2-Clicky-Game/index.html"), {
+// workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/Friends2-Clicky-Game/index.html"), {
   
-  blacklist: [/^\/_/,/\/[^\/?]+\.[^\/]+$/],
-});
+//   blacklist: [/^\/_/,/\/[^\/?]+\.[^\/]+$/],
+// });
